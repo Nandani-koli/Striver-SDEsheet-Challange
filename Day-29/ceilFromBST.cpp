b8ccf959d34c65267ef4595e143a23225041570c
Nandani-koli <70843941+Nandani-koli@users.noreply.github.com>
2022-07-13 22:39:41 +0530

    Create ceilFromBST.cpp

---
+++ Day-29/ceilFromBST.cpp
@@ -0,0 +1,65 @@
+/*
+Ninja is given a binary search tree and an integer. Now he is given a particular key in the tree and returns its ceil value. Can you help Ninja solve the problem?
+Note:
+
+Ceil of an integer is the closest integer greater than or equal to a given number.
+For example:
+arr[] = {1, 2, 5, 7, 8, 9}, key = 3.
+The closest integer greater than 3 in the given array is 5. So, its ceil value in the given array is 5.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode()
+    {
+        if (left)
+        {
+            delete left;
+        }
+        if (right)
+        {
+            delete right;
+        }
+    }
+};
+
+int findCeil(BinaryTreeNode<int> *node, int x)
+{
+    // Write your code here.
+    int ceil = -1;
+    while (node)
+    {
+        if (node->data == x)
+        {
+            ceil = node->data;
+            return ceil;
+        }
+        if (node->data > x)
+        {
+            ceil = node->data;
+            node = node->left;
+        }
+        else
+        {
+            node = node->right;
+        }
+    }
+    return ceil;
+}
